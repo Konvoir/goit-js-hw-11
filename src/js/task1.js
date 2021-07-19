892a78616f63f0594e9de908202ce24794d3e22a
Konvoir <baleskoff@gmail.com>
2021-07-19 13:16:12 +0300

    push

---
+++ src/js/task1.js
@@ -0,0 +1,32 @@
+const startBtn = document.querySelector('[data-start]');
+const stopBtn = document.querySelector('[data-stop]');
+
+
+const timer = {
+    interval: null,
+    isActive: false,
+
+    clickOnStart () {
+        if (this.isActive) {return;}
+        this.isActive = true;
+        startBtn.disabled = true;
+        this.interval = setInterval(chengeBgColor, 1000, 1000);
+    },
+
+    clickOnStop () {
+        clearInterval(this.interval);
+        this.isActive = false;
+        startBtn.disabled = false;
+    }
+ 
+};
+
+startBtn.addEventListener('click', () => {timer.clickOnStart()});
+stopBtn.addEventListener('click', () => {timer.clickOnStop()});
+
+function chengeBgColor () { console.log(document.body.style.background = getRandomHexColor());}
+
+
+function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
