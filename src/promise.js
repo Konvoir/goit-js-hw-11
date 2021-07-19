f2423ff15eb85b18f0de08e437b06b0f01a6404e
Konvoir <baleskoff@gmail.com>
2021-07-19 08:08:01 +0300

    promiseAll

--- src/promise.js
+++ src/promise.js
@@ -2,10 +2,10 @@ const delay = ms => {
     return new Promise((resolve) => setTimeout(() => resolve(ms), ms))
 };
 
-const logger = time => console.log(`Resolved after ${time}ms`);
+const loger = time => console.log(`Resolved after ${time}ms`);
 
 // Вызовы функции для проверки
-delay(2000).then(logger); // Resolved after 2000ms
-delay(1000).then(logger); // Resolved after 1000ms
-delay(1500).then(logger); // Resolved after 1500ms
+delay(2000).then(loger); // Resolved after 2000ms
+delay(1000).then(loger); // Resolved after 1000ms
+delay(1500).then(loger); // Resolved after 1500ms
 
